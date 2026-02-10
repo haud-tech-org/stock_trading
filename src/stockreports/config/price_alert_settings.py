3525d592344b135b4db26289799cc84b9cd62f31
haudtech <haud.tech@gmail.com>
2026-02-10 08:44:47 +0700

    - update configs

--- src/stockreports/config/price_alert_settings.py
+++ src/stockreports/config/price_alert_settings.py
@@ -6,13 +6,13 @@ ALLOW_REPEATED_LEVEL_ALERTS = False
 
 PRICE_ALERTS = {
     "VN30": {
-        "reference_price": 1969.3,
-        "fixed_levels": [1804.57, 1846.13, 1877.78, 1902.37, 1913.03, 1923.6, 1943.18, 1974.36, 1987.27, 1997.86, 2012.03, 2021.85, 2063.27, 2076.97, 2096.88, 2103.31],
+        "reference_price": 1943.6,
+        "fixed_levels": [1730.64, 1739.24, 1768.49, 1798.52, 1805.63, 1828.46, 1839.58, 1848.4, 1861.33, 1871.43, 1880.92, 1892.37, 1903.01, 1912.84, 1924.3, 1932.76, 1943.81, 1965.38, 1976.72, 1992.5, 2002.81, 2017.1, 2029.37, 2054.16, 2063.27, 2076.97, 2096.88, 2103.31],
         "absolute_interval": 9.0,
     },
     "VN30F1M": {
-        "reference_price": 1972.2,
-        "fixed_levels": [1852.56, 1879.48, 1897.5, 1911.6, 1928.46, 1939.87, 1983.3, 1991.07, 2005.32, 2023.62, 2040.73, 2052.2, 2098.72],
+        "reference_price": 1968.2,
+        "fixed_levels": [1736.6, 1773.65, 1798.08, 1829.1, 1839.51, 1853.5, 1863.65, 1875.05, 1883.15, 1896.56, 1906.48, 1915.33, 1928.73, 1941.44, 1953.0, 1983.3, 1991.07, 2004.88, 2024.02, 2039.44, 2052.2, 2098.72],
         "absolute_interval": 9.0,
     }
 }
@@ -33,7 +33,7 @@ PERFORMANCE_BY_APPROACH = {
     'COMPARISON': {'avg_worst_loss_price': 0.0},
     'TREND_REVERSAL': {'avg_worst_loss_price': 1.3},
     'VOLUME_REVERSAL': {'avg_worst_loss_price': 0.8},
-    'SESSION_EXTREME_VOLUME_REVERSAL': {'avg_worst_loss_price': 1.4},
+    'SESSION_EXTREME_VOLUME_REVERSAL': {'avg_worst_loss_price': 1.5},
     'CONSISTENT_VOLUME_ANCHOR': {'avg_worst_loss_price': 1.3}
 }
 
