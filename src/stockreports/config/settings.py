12b69dff1654d8d6565e40984f6c58b821ec01df
haudtech <haud.tech@gmail.com>
2026-03-14 19:17:24 +0700

    - Revert the settings file

--- src/stockreports/config/settings.py
+++ src/stockreports/config/settings.py
@@ -158,16 +158,16 @@ ALERT_APPROACHES = [
 #   }
 SYMBOL_ALERT_APPROACHES = {
     "VN30F1M": [
-        #"CONSISTENT_MOMENTUM",
-        #"STRONG_CANDLE",
-        #"VOLUME_SPIKE_CONFIRMATION",
+        "CONSISTENT_MOMENTUM",
+        "STRONG_CANDLE",
+        "VOLUME_SPIKE_CONFIRMATION",
         "VRA",
-        #"CONSISTENT_VOLUME_ANCHOR", 
-        #"ICHIMOKU"
+        "CONSISTENT_VOLUME_ANCHOR", 
+        "ICHIMOKU"
     ],
     "VN30": [
-        #"VRA",
-        #"CONSISTENT_VOLUME_ANCHOR",
+        "VRA",
+        "CONSISTENT_VOLUME_ANCHOR",
     ],
 }
 
