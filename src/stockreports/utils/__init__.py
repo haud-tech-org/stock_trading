4ca270b7570f43bda4d428631c9c2b55ff73cb18
haudtech <haud.tech@gmail.com>
2026-04-16 16:26:08 +0700

    - Remove redundant methods no longer referenced

--- src/stockreports/utils/__init__.py
+++ src/stockreports/utils/__init__.py
@@ -16,7 +16,6 @@ from .data_utils import (
 
 from .time_utils import (
     TIME_FORMATS,
-    is_trading_hours,
     get_market_timezone_str,
     TIMEZONE_STR,
     SESSIONS,
@@ -41,7 +40,6 @@ __all__ = [
     'load_live_data',
     # from time_utils
     'TIME_FORMATS',
-    'is_trading_hours',
     'get_market_timezone_str',
     'TIMEZONE_STR',
     'SESSIONS',
